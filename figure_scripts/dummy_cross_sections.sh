5d44e37dfb9b04139890806cfc11d4851a06e55b
Matt Gibb <matt@mattgibb.com>
2012-06-25 17:37:13 +0100

    Perfect dummy figures

--- figure_scripts/dummy_cross_sections.sh
+++ figure_scripts/dummy_cross_sections.sh
@@ -4,8 +4,7 @@
 # source constants.sh relative to this file
 . $( cd "$( dirname "$0" )" && pwd )/init.sh
 
-generate_slice()
-{
+generate_noisy_slice() {
   # build arguments
   EXPERIMENT=$1
   ITERATION=$2
@@ -20,12 +19,35 @@ generate_slice()
   flip_and_convert_slice $TIFF_PATH $PDF_PATH
 }
 
+generate_perfect_slice() {
+  # build arguments
+  EXPERIMENT=$1
+  DIM=$2
+  SLICE=$3
+  VOLUME_PATH=results/dummy/perfect_$EXPERIMENT/HiResTransforms_4_32/CenteredAffineTransform/HiRes.mha
+  TIFF_PATH=Ch7/Figs/$SLICE.tiff
+  PDF_PATH=Ch7/Figs/dummies/cross_section_perfect_${EXPERIMENT}_${DIM}_${SLICE}.pdf
+  
+  # generate slices
+  extract_slice $VOLUME_PATH Ch7/Figs $DIM $SLICE
+  flip_and_convert_slice $TIFF_PATH $PDF_PATH
+}
+
 # generate 2 perpendicular cross-sections for each experiment and iteration
 for experiment in 200_alpha0.4{,r,t,rt}; do
+  echo experiment: $experiment
+  
+  # generate noisy slices
   for iteration in {0,1,3,10}; do
-    echo experiment: $experiment, iteration: $iteration
-    generate_slice $experiment $iteration 0 088
-    generate_slice $experiment $iteration 1 107
+    echo iteration: $iteration
+    generate_noisy_slice $experiment $iteration 0 088
+    generate_noisy_slice $experiment $iteration 1 107
     echo
   done
+  
+  # generate perfect slices
+  echo perfect slice
+  generate_perfect_slice $experiment 0 088
+  generate_perfect_slice $experiment 1 107
+  echo
 done
