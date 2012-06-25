5859873cb8c0fa65625b1e60389e5f3e15092c3b
Matt Gibb <matt@mattgibb.com>
2012-06-25 13:40:30 +0100

    Factored out slice extraction

---
+++ figure_scripts/init.sh
@@ -0,0 +1,37 @@
+# define common constants
+THESIS_PATH=$( cd "$( dirname "$0" )/.." && pwd )
+REGISTRATION_PATH="/Users/Matt/Code/imaging/registration/"
+BINARY_PATH="$REGISTRATION_PATH/itk_release/"
+
+# compile binaries
+( cd $BINARY_PATH && make )
+
+# extract slice from volume
+extract_slice() {
+  # build arguments
+  VOLUME_FILE=$1
+  OUTPUT_DIR=$2
+  DIM=$3
+  SLICE=$4
+  
+  $BINARY_PATH/SplitVolumeIntoSlices \
+    $REGISTRATION_PATH/$VOLUME_FILE \
+    $THESIS_PATH/$OUTPUT_DIR -e tiff -l -d $DIM -s $SLICE
+}
+
+# vertical flip raw tiff slices
+# usage: flip_and_convert_slice Ch6/Figs/image.tiff Ch6/Figs/result.pdf
+flip_and_convert_slice() {
+  # build arguments
+  TIFF_PATH=$THESIS_PATH/$1
+  PDF_PATH=$THESIS_PATH/$2
+  
+  # vertical flip, composed of a horizontal flip and 2 90 degree rotations
+  $BINARY_PATH/RotateImage $TIFF_PATH $TIFF_PATH
+  $BINARY_PATH/RotateImage $TIFF_PATH $TIFF_PATH
+  $BINARY_PATH/FlipImage   $TIFF_PATH $TIFF_PATH
+  
+  # convert tiff file to pdf
+  tiff2pdf $TIFF_PATH > $PDF_PATH
+  rm $TIFF_PATH
+}
--- figure_scripts/lores_cross_sections.sh
+++ figure_scripts/lores_cross_sections.sh
@@ -2,30 +2,21 @@
 # script to generate lores transverse slices
 # with and without adjustments
 
-THESIS_PATH=$( cd "$( dirname "$0" )/.." && pwd )
-REGISTRATION_PATH="/Users/Matt/Code/imaging/registration/"
-BINARY_PATH="$REGISTRATION_PATH/itk_release/"
-
-# compile binaries
-( cd $BINARY_PATH && make  )
-
+# source constants.sh relative to this file
+. $( cd "$( dirname "$0" )" && pwd )/init.sh
 
 generate_slice()
 {
+  # build arguments
   VOLUME=$1
   DIM=$2
   SLICE=$3
-  $BINARY_PATH/SplitVolumeIntoSlices \
-    $REGISTRATION_PATH/results/Rat28/affine/ColourResamples_1_8/$VOLUME.mha \
-    $THESIS_PATH/Ch6/Figs -e tiff -l -s $SLICE -d $DIM
+  TIFF_PATH=Ch6/Figs/$SLICE.tiff
+  PDF_PATH=Ch6/Figs/${VOLUME}_${DIM}_${SLICE}.pdf
   
-  TIFF_PATH=$THESIS_PATH/Ch6/Figs/$SLICE.tiff
-  # vertical flip, composed of a horizontal flip and 2 90 degree rotations
-  $BINARY_PATH/RotateImage $TIFF_PATH $TIFF_PATH
-  $BINARY_PATH/RotateImage $TIFF_PATH $TIFF_PATH
-  $BINARY_PATH/FlipImage   $TIFF_PATH $TIFF_PATH
-  tiff2pdf $TIFF_PATH > $THESIS_PATH/Ch6/Figs/${VOLUME}_${DIM}_${SLICE}.pdf
-  rm $THESIS_PATH/Ch6/Figs/$SLICE.tiff
+  # generate slice
+  extract_slice results/Rat28/affine/ColourResamples_1_8/$VOLUME.mha Ch6/Figs $DIM $SLICE
+  flip_and_convert_slice $TIFF_PATH $PDF_PATH
 }
 
 generate_slice LoRes_without_adjustments 0 235
