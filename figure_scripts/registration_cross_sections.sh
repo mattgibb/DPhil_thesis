7545dfeb5d3208c16145d64cf0f0f7ecd685de83
Matt Gibb <matt@mattgibb.com>
2012-07-03 01:18:09 +0100

    Geometric initialisation cut-throughs

--- figure_scripts/registration_cross_sections.sh
+++ figure_scripts/registration_cross_sections.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# script to generate lores transverse slices
-# with and without adjustments
+# script to generate HiRes transverse slices,
+# before and after various registrations
 
 # source constants.sh relative to this file
 . $( cd "$( dirname "$0" )" && pwd )/init.sh
@@ -8,25 +8,26 @@
 generate_slice()
 {
   # build arguments
-  TRANSFORM=$1
+  OUTPUT_DIR=$1
   TRANSFORM_FULL_NAME=$2
   DIM=$3
   SLICE=$4
-  VOLUME_PATH=results/Rat28/$TRANSFORM/HiResTransforms_1_8/$TRANSFORM_FULL_NAME/HiRes.mha
+  VOLUME_PATH=results/Rat28/$OUTPUT_DIR/HiResTransforms_1_8/$TRANSFORM_FULL_NAME/HiRes.mha
   TIFF_PATH=Ch6/Figs/$SLICE.tiff
-  PDF_PATH=Ch6/Figs/${TRANSFORM}_${DIM}_${SLICE}.pdf
+  PDF_PATH=Ch6/Figs/${OUTPUT_DIR}_${DIM}_${SLICE}.pdf
   
   # generate slice
   extract_slice $VOLUME_PATH Ch6/Figs $DIM $SLICE
   flip_and_convert_slice $TIFF_PATH $PDF_PATH
 }
 
-transforms=(rigid size affine)
-transform_full_names=(Centered{{Rigid,Similarity}2D,Affine}Transform)
+
+output_dirs=(geometric rigid size affine)
+transforms=(Centered{{Rigid{,},Similarity}2D,Affine}Transform)
 slices=(235 287)
 
-for i in {0..2}; do
+for i in {0..3}; do
   for dim in 0 1; do
-    generate_slice ${transforms[i]} ${transform_full_names[i]} $dim ${slices[$dim]}
+    generate_slice ${output_dirs[i]} ${transforms[i]} $dim ${slices[$dim]}
   done
 done
