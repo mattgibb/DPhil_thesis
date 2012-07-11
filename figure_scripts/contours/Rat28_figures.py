31b73007d24c6518a655653da6d79dfc6c29e8df
Matt Gibb <matt@mattgibb.com>
2012-07-11 13:58:06 +0100

    Generate LoRes contour figures

--- figure_scripts/contours/Rat28_figures.py
+++ figure_scripts/contours/Rat28_figures.py
@@ -11,51 +11,55 @@ paraview.simple._DisableFirstRenderCameraReset()
 view = create_render_view()
 view.ViewSize = [3000, 2000] #[width, height]
 
-def save_positive_x_snapshot(name):
+def positive_x_snapshot():
     view.CameraPosition = [-17397.295366965835, 7634.2001953125, 4500.0]
     view.CameraFocalPoint = [6237.7001953125, 7634.2001953125, 4500.0]
     view.CameraParallelScale = 10836.969887782794
     view.CenterOfRotation = [6237.7001953125, 7634.2001953125, 4500.0]
     view.CameraClippingRange = [10985.622217983677, 39614.95948497032]
     view.CameraViewUp = [0.0, 0.0, 1.0]
-    
-    # save figures
-    WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/whole_positive_x_%s.png" % name )
 
-def save_negative_x_snapshot(name):
+def negative_x_snapshot():
     view.CameraPosition = [29872.695757590835, 7634.2001953125, 4500.0]
     view.CameraFocalPoint = [6237.7001953125, 7634.2001953125, 4500.0]
     view.CameraParallelScale = 10836.969887782794
     view.CenterOfRotation = [6237.7001953125, 7634.2001953125, 4500.0]
     view.CameraClippingRange = [10985.622217983677, 39614.95948497032]
     view.CameraViewUp = [0.0, 0.0, 1.0]
-    
-    # save figures
-    WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/whole_negative_x_%s.png" % name )
 
-def save_positive_y_snapshot(name):
+def positive_y_snapshot():
     view.CameraPosition = [6237.7001953125, -16000.795366965835, 4500.0]
     view.CameraFocalPoint = [6237.7001953125, 7634.2001953125, 4500.0]
     view.CameraParallelScale = 10836.969887782794
     view.CenterOfRotation = [6237.7001953125, 7634.2001953125, 4500.0]
     view.CameraClippingRange = [8206.587217983677, 43113.191984970326]
     view.CameraViewUp = [0.0, 0.0, 1.0]
-    
-    # save figures
-    WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/whole_positive_y_%s.png" % name )
 
-def save_positive_z_snapshot(name):
+def negative_y_snapshot():
+    view.CameraPosition = [6237.7001953125, 31269.195757590835, 4500.0]
+    view.CameraFocalPoint = [6237.7001953125, 7634.2001953125, 4500.0]
+    view.CameraParallelScale = 10836.969887782794
+    view.CenterOfRotation = [6237.7001953125, 7634.2001953125, 4500.0]
+    view.CameraClippingRange = [8206.587217983677, 43113.191984970326]
+    view.CameraViewUp = [0.0, 0.0, 1.0]
+
+def positive_z_snapshot():
     view.CameraPosition = [6237.7001953125, 7634.200195312501, -24098.344630356787]
     view.CameraFocalPoint = [6237.7001953125, 7634.200195312501, 4500.0]
     view.CameraParallelScale = 10836.969887782794
     view.CenterOfRotation = [6237.7001953125, 7634.2001953125, 4500.0]
     view.CameraClippingRange = [19357.361184053218, 40299.81979981215]
     view.CameraViewUp = [1.0, 0.0, 0.0]
-    
-    # save figures
-    WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/whole_positive_z_%s.png" % name )
 
-def save_zoom_snapshot(name):
+def negative_z_snapshot():
+    view.CameraPosition = [6237.7001953125, 7634.2001953125, 33098.344630356776]
+    view.CameraFocalPoint = [6237.7001953125, 7634.2001953125, 4500.0]
+    view.CameraParallelScale = 10836.969887782794
+    view.CenterOfRotation = [6237.7001953125, 7634.2001953125, 4500.0]
+    view.CameraClippingRange = [19357.361184053207, 40299.819799812125]
+    view.CameraViewUp = [1.0, 0.0, 0.0]
+
+def zoom_snapshot():
     view.CameraPosition = [-9905.319791217333, 7634.2001953125, 4500.0]
     view.CameraFocalPoint = [6237.7001953125, 7634.2001953125, 4500.0]
     view.CameraParallelScale = 10836.969887782794
@@ -63,14 +67,33 @@ def save_zoom_snapshot(name):
     view.CameraClippingRange = [3568.5663979926585, 32010.604275585596]
     
     # save figures
-    WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/zoom_%s.png" % name )
+    # WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/zoom_%s.png" % name )
 
-def save_snapshots(name):
-    save_positive_x_snapshot(name)
-    save_negative_x_snapshot(name)
-    save_positive_y_snapshot(name)
-    save_positive_z_snapshot(name)
-    
+def save_HiRes_snapshots(name):
+    positive_x_snapshot()
+    WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/whole_positive_x_%s.png" % name )
+    negative_x_snapshot()
+    WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/whole_negative_x_%s.png" % name )
+    positive_y_snapshot()
+    WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/whole_positive_y_%s.png" % name )
+    positive_z_snapshot()
+    WriteImage( thesis_root + "Ch7/Figs/Rat28/contours/whole_positive_z_%s.png" % name )
+
+def save_LoRes_snapshots():
+    positive_x_snapshot()
+    WriteImage( thesis_root + "Ch6/Figs/Rat28/contours/LoRes_positive_x.png")
+    negative_x_snapshot()
+    WriteImage( thesis_root + "Ch6/Figs/Rat28/contours/LoRes_negative_x.png")
+    positive_y_snapshot()
+    WriteImage( thesis_root + "Ch6/Figs/Rat28/contours/LoRes_positive_y.png")
+    negative_y_snapshot()
+    WriteImage( thesis_root + "Ch6/Figs/Rat28/contours/LoRes_negative_y.png")
+    positive_z_snapshot()
+    WriteImage( thesis_root + "Ch6/Figs/Rat28/contours/LoRes_positive_z.png")
+    negative_z_snapshot()
+    WriteImage( thesis_root + "Ch6/Figs/Rat28/contours/LoRes_negative_z.png")
+
+# HiRes contours
 for name, directory in segmentation_dirs.items():
     # load contours
     contour   = XMLPolyDataReader(guiName="noisy_contour",
@@ -87,7 +110,26 @@ for name, directory in segmentation_dirs.items():
     Render()
     
     # save snapshots
-    save_snapshots(name)
+    save_HiRes_snapshots(name)
     
     # clean up
     Delete(contour)
+
+# LoRes contour
+LoRes_contour_path = Rat28_root + "affine/ColourResamples_1_8/LoRes_segmentation_removed_label_objects_contour.vtp"
+contour   = XMLPolyDataReader(guiName="contour",
+                              PointArrayStatus=['MetaImage', 'Normals'],
+                              CellArrayStatus=[],
+                              FileName=[LoRes_contour_path] )
+            
+dp = set_display_properties(contour)
+            
+# display results
+Show(contour)
+Render()
+
+# save snapshots
+save_LoRes_snapshots()
+
+# clean up
+Delete(contour)
